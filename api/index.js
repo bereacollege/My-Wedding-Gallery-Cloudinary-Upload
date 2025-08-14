effd890f87d51a179f9376acc94fec102bc7aa85
bereacollege <chukwuemekaogbonnaya95@gmail.com>
2025-08-14 14:39:10 +0100

    forget one line on index.js

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ const app = express();
 cloudinary.config({
     cloud_name: import.meta.env.VITE_CLOUDINARY_CLOUD_NAME,
     api_key: import.meta.env.VITE_CLOUDINARY_API_KEY,
-    api_secret: process.env.VITE_CLOUDINARY_API_SECRET
+    api_secret: import.meta.env.VITE_CLOUDINARY_API_SECRET
 });
 
 // Enable CORS
